dfd5b3d06228c32f4c40828032ba97171a4ecd03
Max-1820 <80045506+Max-1820@users.noreply.github.com>
2021-10-19 18:32:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+nosex=0;
+nosey=0;
+difference=0;
+rightWristX=0;
+leftwristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,80)
+
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized!');
+}
+
+function draw(){
+    document.getElementById("square_side").innerHTML="Width and height of a square will be="+difference+"px";
+    background('#969A97');
+    fill('#F90093');
+    stroke('#F90093');
+    square(nosex, nosey, difference);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results)
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log("nosex="+nosex+" nosey=" +nosey);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX=" + leftWristX+"rightWristX"+rightWristX+"difference="+difference);
+    }
+
+}
